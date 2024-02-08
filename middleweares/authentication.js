3ad97a23b45062afdcc7643754847e4a093a963e
giacomotolari <tolarigiacomo@gmail.com>
2024-02-08 12:07:38 +0100

    feature: complete task 2, refactor: authentication with regex

--- middleweares/authentication.js
+++ middleweares/authentication.js
@@ -1,8 +1,13 @@
 export function authentication(req, res, next) {
-    if (!req.body.username || !req.body.password) {
-      res.status(400).json({ message: "Username and password are required!" });
-      return;
-    }
-    next();
+  const { username, password } = req.body;
+  const isValideUsername = username.length > 4 && username.length <= 20;
+  const isValidePassword = /^(?=.*[A-Z])(?=.*[0-9])(?=^\S+$).{8,}$/.test(
+    password
+  );
+
+  if (!isValideUsername || !isValidePassword) {
+    res.status(400).json({ message: "Username and password are wrong!" });
+    return;
   }
-  
+  next();
+}
